d766739e608e176569dcd4e2d1d0f17dfc6d2c6c
Panfilov Valerian <>
2026-01-30 18:59:24 +0300

    нормальный вывод  статики в корне

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
-import os
 
 app = FastAPI()
 
-# Подключение статических файлов
-app.mount("/", StaticFiles(directory="design1"), name="static")
-
-#@app.get("/")
-#async def read_root():
-#    return HTMLResponse(open("design1/index.html").read())
+# Обслуживание статики и автоматическая выдача index.html на "/"
+app.mount("/", StaticFiles(directory="design1", html=True), name="static")
 
