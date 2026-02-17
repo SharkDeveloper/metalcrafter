e03a40e0d737e424e839be9f30a5925d64170fef
Panfilov Valerian <>
2026-02-17 17:19:59 +0300

    Добавление картинок и описания проектов

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
 # Обслуживание статики и автоматическая выдача index.html на "/"
-app.mount("/", StaticFiles(directory="design1", html=True), name="static")
+app.mount("/", StaticFiles(directory="templates", html=True), name="static")
