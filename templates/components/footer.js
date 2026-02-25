6b27364231da5886d484b2466f9c483be057013d
SharkDeveloper <48812383+SharkDeveloper@users.noreply.github.com>
2026-02-25 06:17:49 +0000

    добавление питерского адреса

--- templates/components/footer.js
+++ templates/components/footer.js
@@ -150,9 +150,10 @@
             <div class="footer-column">
               <h3>Контакты</h3>
               <ul>
-                <li style="color: #9ca3af; margin-bottom: 0.75rem;">📞 +7 (906) 551-77-01</li>
+                <li style="color: #9ca3af; margin-bottom: 0.75rem;">📞 +7 (909) 371-79-93</li>
                 <li style="color: #9ca3af; margin-bottom: 0.75rem;">✉️ info@metalcraft.pro</li>
                 <li style="color: #9ca3af; margin-bottom: 0.75rem;">🏢 Тверь, Старицкое Шоссе, д. 25с3</li>
+                <li style="color: #9ca3af; margin-bottom: 0.75rem;">🏢 Санкт-Петербург, Пушкинский район, поселок Шушары, 1-й Бадаевский проезд, д. 1Б</li>
                 <li style="color: #9ca3af;">🕒 Ежедневно: 10:00 - 21:00</li>
               </ul>
             </div>
