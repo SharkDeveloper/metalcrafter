cd2ba83b335ab1ea0520b2bf07baf8def41afcc0
Panfilov Valerian <>
2026-03-09 23:34:12 +0300

    подготовка к проду

---
+++ wsgi.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+activate_this = os.path.expanduser('~/metalcraft/venv/bin/activate_this.py')
+exec(open(activate_this).read(), {'__file__': activate_this})
+
+sys.path.insert(1, os.path.expanduser('~/metalcraft/public_html/'))
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'metalcraft.settings')
+
+application = get_wsgi_application()
