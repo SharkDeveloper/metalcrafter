dd846d9baa63c00d50ecfd50677b73b9c804a7f5
SharkDeveloper <48812383+SharkDeveloper@users.noreply.github.com>
2026-03-12 14:24:28 +0000

    Корректировака формирования письма

--- main/smtp_sender.py
+++ main/smtp_sender.py
@@ -13,11 +13,11 @@ SMTP_HOST = os.getenv("SMTP_HOST")
 SMTP_PORT = os.getenv("SMTP_PORT")
 
 
-def send_mail(order_id=0, company: str, name: str, phone: str, email: str, message:str ) -> None:
+def send_mail(order_id=1, company: str, name: str, phone: str, email: str, description:str ) -> None:
   msg = EmailMessage()
   msg['Subject'] = f'Заказ №{order_id}'
 
-  msg.set_content(picture_info+"\n\n"+comment+"\n\n"+contacts)
+  msg.set_content(company + "\n\n Имя заказчкика: " + name + "\n\n Номер телефона: " + phone + "\n\n email: " + email + "\n\n Описание проекта" + description)
 
   with SMTP(SMTP_HOST,int(SMTP_PORT)) as smtp:
     smtp.ehlo()
@@ -25,5 +25,5 @@ def send_mail(order_id=0, company: str, name: str, phone: str, email: str, messa
     smtp.ehlo()
     smtp.login(EMAIL_LOGIN, EMAIL_PASSWORD)
     smtp.send_message(msg,EMAIL_LOGIN,EMAIL_LOGIN)
-    print('Message sent!')
-order_id++
+    print('Message sent')
+  order_id+=1
