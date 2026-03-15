fbd01433f908f3fd15a3716f0396c026a8cdbb7b
SharkDeveloper <48812383+SharkDeveloper@users.noreply.github.com>
2026-03-15 20:28:20 +0300

    Update wsgi.py

--- public_html/wsgi.py
+++ public_html/wsgi.py
@@ -1,9 +1,6 @@
 import os
 import sys
 
-activate_this = os.path.expanduser('~/metalcraft/venv/bin/activate_this.py')
-exec(open(activate_this).read(), {'__file__': activate_this})
-
 sys.path.insert(1, os.path.expanduser('~/public_html/'))
 
 from django.core.wsgi import get_wsgi_application
